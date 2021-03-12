3f8f201ca02828141ba474b83824f6556b2e484e
Elena-Weber <crochetmiracles@gmail.com>
2021-03-11 21:42:26 -0500

    commit tree dac95bb955c0655aac34da3470fd9fe4014442f8

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
 class Triangle
-  # write code here
+
+  attr_accessor :length1, :length2, :length3
+
+  @@all = []
+
+  def initialize(length1, length2, length3)
+@length1 = length1
+@length2 = length2
+@length3 = length3
+@@all << self
+  end
+
+  def kind
+    if length1 > 0 && length2 > 0 && length3 > 0
+      if length1 + length2 > length3 && length1 + length3 > length2 && length3 + length2 > length1
+        if length1 == length2 && length2 == length3
+        :equilateral
+        elsif length1 == length2 || length1 == length3 || length2 == length3
+        :isosceles
+        else
+        :scalene
+        end
+      else
+        begin
+            raise TriangleError
+            puts error.message
+        end
+      end
+    else
+      begin
+        raise TriangleError
+        puts error.message
+    end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Please enter correct triangle dimensions"
+    end
+  end
+
 end
